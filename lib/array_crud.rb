846af8ff336ec4c4199c435a501c52b0fe767b77
Treven Womack <keytonwomack@yahoo.com>
2019-11-17 03:25:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,31 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  array=[1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array=[1,2,3,4]
+  array.push("arrays!")
+  p array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array=[1,2,3,4]
+  array.unshift("wow")
+  p array
 end
 
 def remove_element_from_end_of_array(array)
-  
+  "arrays!" array.pop
+  p array
 end
 
 def remove_element_from_start_of_array(array)
-  
+  "wow"=array.push
+  p array
 end
 
 def retrieve_element_from_index(array, index_number)
